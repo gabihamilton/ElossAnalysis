968d50047bc513572563b517f92305bc7dc2c60b
Gabriela Hamilton <gbibim@MacBook-Air-de-Gabriela.local>
2021-07-24 16:29:37 -0500

    smoran data

--- 1DACC.cxx
+++ 1DACC.cxx
@@ -118,12 +118,15 @@ int main(int argc, char **argv)
   TCut Phi_cut_S = Form("PhiPQ>%f && PhiPQ<%f", Phi_min, Phi_max);
   #if SMORAN==1
     TCut Pt2_cut_S = Form("Pt2>%f && Pt2<%f", Pt2_min, Pt2_max);
+    TCut cuts_simulD = Q2_cut_S&&Nu_cut_S&&Phi_cut_S&&Pt2_cut_S;
+    TCut cuts_simulS = Q2_cut_S&&Nu_cut_S&&Phi_cut_S&&Pt2_cut_S;
   #else
     TCut Pt2_cut_S = Form("Pt*Pt>%f && Pt*Pt<%f", Pt2_min, Pt2_max);
+    TCut cuts_simulD = Target_cutD&&Q2_cut_S&&Nu_cut_S&&Phi_cut_S&&Pt2_cut_S;
+    TCut cuts_simulS = Target_cutS&&Q2_cut_S&&Nu_cut_S&&Phi_cut_S&&Pt2_cut_S;
   #endif
 
-  TCut cuts_simulD = Target_cutD&&Q2_cut_S&&Nu_cut_S&&Phi_cut_S&&Pt2_cut_S;
-  TCut cuts_simulS = Target_cutS&&Q2_cut_S&&Nu_cut_S&&Phi_cut_S&&Pt2_cut_S;
+
   
 
   TCut xf_cut = "Xf>0.1"; //  Typical xF cut
